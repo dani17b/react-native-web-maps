821b9ead8d15f67da4a9076bca05acdeda3618ad
Dani <daniel.anido17@gmail.com>
2019-02-22 19:52:00 +0100

    Improvements at marker

--- dist/Marker.js
+++ dist/Marker.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 import { Marker } from 'react-google-maps';
+import MarkerWithLabel from "react-google-maps/lib/components/addons/MarkerWithLabel";
 
 class MapViewMarker extends Component {
   render() {
+    if(this.props.children){
+      return (
+        <MarkerWithLabel
+          draggable={this.props.draggable}
+          onDragEnd={this.props.onDragEnd}
+          title={this.props.description ? `${this.props.title}\n${this.props.description}` : this.props.title}
+          position={{ lat: this.props.coordinate.latitude, lng: this.props.coordinate.longitude }}
+          opacity={typeof this.props.opacity != "undefined" ? this.props.opacity : 1}
+        >
+          {this.props.children}
+        </MarkerWithLabel>
+      );
+    }
+
     return (
       <Marker
         draggable={this.props.draggable}
         onDragEnd={this.props.onDragEnd}
         title={this.props.description ? `${this.props.title}\n${this.props.description}` : this.props.title}
         position={{ lat: this.props.coordinate.latitude, lng: this.props.coordinate.longitude }}
+        opacity={typeof this.props.opacity != "undefined" ? this.props.opacity : 1}
       >
         {this.props.children}
       </Marker>
