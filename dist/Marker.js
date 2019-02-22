e514c957111141fb61b65595ce2cf40fb4c79e04
Dani <daniel.anido17@gmail.com>
2019-02-22 21:31:49 +0100

    Fixes

--- dist/Marker.js
+++ dist/Marker.js
@@ -3,17 +3,46 @@ import { Marker } from 'react-google-maps';
 import MarkerWithLabel from "react-google-maps/lib/components/addons/MarkerWithLabel";
 
 class MapViewMarker extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.makerWithLabel = React.createRef();
+    this.makerWithLabelChild = React.createRef();
+
+    this.state = {
+      labelCenterPoint : {
+        x : 0,
+        y : 0
+      }
+    }
+  }
+
+  componentDidMount(){
+    if(this.props.children){
+      let _this = this;
+      setTimeout(function(){
+        _this.setState({
+          labelCenterPoint : {
+            x : _this.makerWithLabelChild.current.offsetWidth / 2,
+            y : _this.makerWithLabelChild.current.offsetHeight / 2
+          }
+        })
+      }, 300);
+    }
+  }
+
   render() {
     if(this.props.children){
       return (
         <MarkerWithLabel
-          draggable={this.props.draggable}
-          onDragEnd={this.props.onDragEnd}
-          title={this.props.description ? `${this.props.title}\n${this.props.description}` : this.props.title}
           position={{ lat: this.props.coordinate.latitude, lng: this.props.coordinate.longitude }}
-          opacity={typeof this.props.opacity != "undefined" ? this.props.opacity : 1}
+          labelAnchor={new google.maps.Point(this.state.labelCenterPoint.x, this.state.labelCenterPoint.y)}
+          opacity={0}
         >
-          {this.props.children}
+          <div ref={this.makerWithLabelChild}>
+            {this.props.children}
+          </div>
         </MarkerWithLabel>
       );
     }
@@ -26,7 +55,6 @@ class MapViewMarker extends Component {
         position={{ lat: this.props.coordinate.latitude, lng: this.props.coordinate.longitude }}
         opacity={typeof this.props.opacity != "undefined" ? this.props.opacity : 1}
       >
-        {this.props.children}
       </Marker>
     );
   }
